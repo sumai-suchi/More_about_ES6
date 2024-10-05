a9f511af7f9a8e3061cb5135d36eeeacb834e647
sumaiya <sumaiyamoina@gmail.com>
2024-10-05 23:51:00 +0600

    adding all the program of this module

--- find.js
+++ find.js
@@ -1,7 +1,14 @@
 //find is as same us filter but it only returns first element which fullfill the condition.
 
-const numbers=[2,3,4,5,778,54,324,65];
+// const numbers=[2,3,4,5,778,54,324,65];
 
-const selected=numbers.find(n=> n>100);
+// const selected=numbers.find(n=> n>100);
 
-console.log(selected);
+// console.log(selected);
+
+// const nums = [1,2,3,4,5];
+// let output = nums.filter(n => n%2);
+// console.log(output);
+
+const [a, b] = [1,2,3,4,45,5]; 
+console.log(a+b);
